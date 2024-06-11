194bd3500bb6bd12974fb7c472a489e948be8190
John Corrigan <jcorrigan322@gmail.com>
2024-06-11 15:01:22 -0700

    file transfers working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import { NextUIProvider } from "@nextui-org/react";
 
+import {
+  CPUStatus,
+  DiskStatus,
+  MemoryStatus,
+} from "../components/Status";
 import FileUpload from "../components/FileUpload";
-import { CPUStatus } from "../components/Status";
 
 export default async function Home() {
-
-    return (
-        <NextUIProvider>
-            <main className="min-h-screen p-24">
-                <div className="w-full flex gap-24 justify-center">
-                        <CPUStatus />
-                </div>
-            </main>
-        </NextUIProvider >
-    );
+  return (
+    <NextUIProvider>
+      <main className="min-h-screen p-24">
+        <div className="w-full flex gap-24 justify-center">
+          <CPUStatus />
+          <MemoryStatus />
+          <DiskStatus />
+        </div>
+       <FileUpload />
+      </main>
+    </NextUIProvider>
+  );
 }
